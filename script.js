ccc330785c619d3f65fb94d5914f65c0bbb1ca39
jotaefe21 <105383250+jotaefe21@users.noreply.github.com>
2022-06-01 16:22:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ window.addEventListener("load", function(){
             document.querySelector(".popup2").style.display = "block";
             document.querySelector(".popup3").style.display = "block";
             document.querySelector(".popup4").style.display = "block";
+            document.querySelector(".popup5").style.display = "block";
+            document.querySelector(".popup6").style.display = "block";
+            document.querySelector(".popup7").style.display = "block";
+            document.querySelector(".popup8").style.display = "block";
         },
         0
     )
@@ -22,6 +26,20 @@ document.querySelector("#close3").addEventListener("click", function(){
 
 document.querySelector("#close4").addEventListener("click", function(){
     document.querySelector(".popup4").style.display = "none";
+    });
+document.querySelector("#close5").addEventListener("click", function(){
+    document.querySelector(".popup5").style.display = "none";
+    });
+
+document.querySelector("#close6").addEventListener("click", function(){
+    document.querySelector(".popup6").style.display = "none";
+    });
+document.querySelector("#close7").addEventListener("click", function(){
+    document.querySelector(".popup7").style.display = "none";
+    });
+
+document.querySelector("#close8").addEventListener("click", function(){
+    document.querySelector(".popup8").style.display = "none";
     });
 
 
