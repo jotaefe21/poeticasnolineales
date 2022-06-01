e9990a08d9cda1551d1b7da134d7e90ad0f8c74d
jotaefe21 <105383250+jotaefe21@users.noreply.github.com>
2022-06-01 15:46:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,27 @@ window.addEventListener("load", function(){
         function open(event){
             document.querySelector(".popup1").style.display = "block";
             document.querySelector(".popup2").style.display = "block";
+            document.querySelector(".popup3").style.display = "block";
+            document.querySelector(".popup4").style.display = "block";
         },
         0
     )
 });
 document.querySelector("#close1").addEventListener("click", function(){
     document.querySelector(".popup1").style.display = "none";
-});
+    });
 
 document.querySelector("#close2").addEventListener("click", function(){
     document.querySelector(".popup2").style.display = "none";
-});
+    });
+document.querySelector("#close3").addEventListener("click", function(){
+    document.querySelector(".popup3").style.display = "none";
+    });
+
+document.querySelector("#close4").addEventListener("click", function(){
+    document.querySelector(".popup4").style.display = "none";
+    });
+
+
+
+    
